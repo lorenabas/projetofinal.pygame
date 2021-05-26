35f3bc9ab09501f060fb494cf03345d5d11100bf
juliacfigueiredo <julia.figueiredo2003@icloud.com>
2021-05-26 15:26:25 -0300

    Commit 2

--- PyGame.py
+++ PyGame.py
@@ -13,3 +13,23 @@ clock = pygame.time.Clock()
 # Fonte para o texto do jogo
 pygame.font.init()
 font = pygame.font.SysFont('comicsans', 75)
+
+class Game:
+    #FPS
+    FPS = 60    
+    
+    #Inicializador para classe de jogo para configurar largura, altura e título
+    def __init__(self, image_path, título, altura, peso):
+        self.título = título
+        self.altura = altura
+        self.peso = peso
+
+        #Cria a janela de tamanho especificado para exibir o jogo
+        self.game_screen = pygame.display.set_mode((altura, peso))
+        #set the game window color to white
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(título)
+
+        # Carregar e definir a imagem de fundo
+        background_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(background_image, (altura, peso))
